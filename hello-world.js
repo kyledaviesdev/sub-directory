016ad365876a78e42b92c5a7fbb8fdd5ef20501a
kyledaviesdev <kylemdavies127@gmail.com>
2025-01-11 10:11:53 -0700

    Dragging

---
+++ hello-world.js
@@ -0,0 +1,21 @@
+const dragElement = document.getElementById('drag-it');
+
+let isDragging = false;
+let offsetX, offsetY;
+
+dragElement.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  offsetX = e.clientX - dragElement.offsetLeft;
+  offsetY = e.clientY - dragElement.offsetTop;
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (isDragging) {
+    dragElement.style.left = (e.clientX - offsetX) + 'px';
+    dragElement.style.top = (e.clientY - offsetY) + 'px';
+  }
+});
